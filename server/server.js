b7a5dab615f92b1cff78c9a3d940f5a33ad58e83
ManousakisG <giwrgozm1995@hotmail.gr>
2021-03-07 21:53:26 +0200

    Setup production build and server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+//Express framework
+const path =require('path');
+const express =require('express');
+const app=express();
+const publicPath = path.join(__dirname, '..', 'public'); //join current directory with public directory
+const port =process.env.PORT || 3000; //Heroku remote 
+app.use(express.static(publicPath)); //use public directory
+
+app.get('*', (req, res) => { // if something then give index.html
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => { //port 3000 is available in all OS.
+    console.log('Server is up!!');
+});
