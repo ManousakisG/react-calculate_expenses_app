5272f00cf6a32ed46a56c2342ec210094d4c7bef
ManousakisG <giwrgozm1995@hotmail.gr>
2021-03-07 15:50:03 +0200

    remove expense data from app.js''

--- src/app.js
+++ src/app.js
@@ -12,18 +12,18 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water bill', amount:500 }));
+/* store.dispatch(addExpense({description: 'Water bill', amount:500 }));
 store.dispatch(addExpense({description: 'Gas bill'}));
-store.dispatch(addExpense({description: 'Rent', amount:12500 }));
+store.dispatch(addExpense({description: 'Rent', amount:12500 })); */
 //store.dispatch(setTextFilter('water'));
 /* 
 setTimeout(() => {
     store.dispatch(setTextFilter('bill'));
 }, 3000) // after 3 seconds there appears rent! */
 
-const state = store.getState();
+/* const state = store.getState();
 const visibleExpenses =getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+console.log(visibleExpenses); */
 
 const jsx = (  // Provide store to all of my components ! With react-redux!
     <Provider store={store}>
